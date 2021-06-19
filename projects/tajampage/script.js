6154725fd14e056d9082391d864eb227735955db
Dmitry Shagurov <shagurovda@gmail.com>
2021-06-19 17:31:35 +0300

    tajam page (wip): slider-2 css, js added; Uploaded more pics for slider-2 markers

--- projects/tajampage/script.js
+++ projects/tajampage/script.js
@@ -7,13 +7,13 @@ var sliderOneItemsArray = Array.from(sliderOne.querySelectorAll(".slider__item")
 
 
 //slider-1 marker click handler
-function toggleSliderOne() {
+function toggleSliderOneByMarker() {
   // Active-marker interaction will not processing
   if (this.classList.contains("active")) {
     return;
   }
 
-  // Make the current marker and its corresponding item inactive
+  // Make the current active-marker and its corresponding item inactive
   let activeMarker = sliderOne.querySelector(".slider__marker.active");
   let activeItem = sliderOne.querySelector(".slider__item.active");
   activeMarker.classList.toggle("active");
@@ -28,5 +28,59 @@ function toggleSliderOne() {
 
 
 for (let marker of sliderOneMarkersArray) {
-  marker.addEventListener("click", toggleSliderOne);
+  marker.addEventListener("click", toggleSliderOneByMarker);
+}
+
+
+
+/* slider-2 interaction */
+
+var sliderTwo = document.querySelector(".slider-2");
+var sliderTwoMarkersArray = Array.from(sliderTwo.querySelectorAll(".slider__marker"));
+var sliderTwoItemsArray = Array.from(sliderTwo.querySelectorAll(".slider__item"));
+
+//slider-2 marker click handler
+function toggleSliderTwoByMarker() {
+  // Active-marker interaction will not processing
+  if (this.classList.contains("active")) {
+    return;
+  }
+
+  // Make the current active-marker and its corresponding item inactive
+  let activeMarker = sliderTwo.querySelector(".slider__marker.active");
+  let activeItem = sliderTwo.querySelector(".slider__item.active");
+  activeMarker.classList.toggle("active");
+  activeItem.classList.toggle("active");
+
+  // Make the clicked marker and its corresponding item active
+  let targetIndex = sliderTwoMarkersArray.indexOf(this);
+  let targetItem = sliderTwoItemsArray[targetIndex];
+  this.classList.toggle("active");
+  targetItem.classList.toggle("active");
+
+  // Clear all marker position classes
+  for (let marker of sliderTwoMarkersArray) {
+    if (marker.classList.contains("first")) {
+      marker.classList.remove("first");
+    } else if (marker.classList.contains("second")) {
+      marker.classList.remove("second");
+    } else if (marker.classList.contains("third")) {
+      marker.classList.remove("third");
+    } else if (marker.classList.contains("fourth")) {
+      marker.classList.remove("fourth");
+    }
+  }
+
+  // Set new marker position classes
+  let markersArrayCopy = sliderTwoMarkersArray.slice();
+  markersArrayCopy.splice(targetIndex, 1);
+  markersArrayCopy[0].classList.add("first");
+  markersArrayCopy[1].classList.add("second");
+  markersArrayCopy[2].classList.add("third");
+  markersArrayCopy[3].classList.add("fourth");
+}
+
+
+for (let marker of sliderTwoMarkersArray) {
+  marker.addEventListener("click", toggleSliderTwoByMarker);
 }
