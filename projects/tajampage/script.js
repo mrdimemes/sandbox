7c5a02bcdbf542bbc21949683326aebed7b0700e
Dmitry Shagurov <shagurovda@gmail.com>
2021-07-16 14:20:57 +0300

    tajam page: sliders touchscreen interactions

--- projects/tajampage/script.js
+++ projects/tajampage/script.js
@@ -15,6 +15,29 @@ headerBurger.addEventListener("click", toggleHeaderBurger);
 
 
 
+/* general sliders interaction */
+
+var sliderTouchStartX;
+var sliderTouchEndX;
+var sliderSwipeLength;
+var sliderIsTouching;
+
+
+function sliderSwipeStart() {
+  sliderTouchStartX = event.touches[0].clientX;
+  sliderTouching = true;
+}
+
+function sliderSwipeMove() {
+  sliderTouchEndX = event.touches[0].clientX;
+}
+
+function sliderSwipeCancel() {
+  sliderTouching = false;
+}
+
+
+
 /* slider-1 interaction */
 
 var sliderOne = document.querySelector(".slider-1");
@@ -47,6 +70,71 @@ for (let marker of sliderOneMarkersArray) {
   marker.addEventListener("click", toggleSliderOneByMarker);
 }
 
+// slider-1 touchscreen interaction
+
+function sliderOneSwipeEnd() {
+  if (!sliderTouching) {
+    return;
+  }
+
+
+  sliderSwipeLength  = sliderTouchEndX - sliderTouchStartX;
+
+  if (Math.abs(sliderSwipeLength) > (0.33 * document.documentElement.clientWidth)) {
+    if (sliderSwipeLength > 0) {
+      sliderOneNext();
+    } else {
+      sliderOnePrevious();
+    }
+  }
+
+  sliderTouching = false;
+}
+
+function sliderOneNext() {
+  // Make the current active-marker and its corresponding item inactive
+  let activeMarker = sliderOne.querySelector(".slider__marker.active");
+  let activeItem = sliderOne.querySelector(".slider__item.active");
+  activeMarker.classList.toggle("active");
+  activeItem.classList.toggle("active");
+
+  // Get index of next item
+  let activeIndex = sliderOneMarkersArray.indexOf(activeMarker);
+  let targetIndex = (activeIndex + 1) % sliderOneMarkersArray.length;
+
+  // Make the target marker and its corresponding item active
+  let targetMarker = sliderOneMarkersArray[targetIndex];
+  let targetItem = sliderOneItemsArray[targetIndex];
+  targetMarker.classList.toggle("active");
+  targetItem.classList.toggle("active");
+}
+
+function sliderOnePrevious() {
+  // Make the current active-marker and its corresponding item inactive
+  let activeMarker = sliderOne.querySelector(".slider__marker.active");
+  let activeItem = sliderOne.querySelector(".slider__item.active");
+  activeMarker.classList.toggle("active");
+  activeItem.classList.toggle("active");
+
+  // Get index of next item
+  let activeIndex = sliderOneMarkersArray.indexOf(activeMarker);
+  let targetIndex = (((activeIndex - 1) % sliderOneMarkersArray.length)
+    + sliderOneMarkersArray.length) % sliderOneMarkersArray.length;
+
+  // Make the target marker and its corresponding item active
+  let targetMarker = sliderOneMarkersArray[targetIndex];
+  let targetItem = sliderOneItemsArray[targetIndex];
+  targetMarker.classList.toggle("active");
+  targetItem.classList.toggle("active");
+}
+
+
+sliderOne.addEventListener("touchstart", sliderSwipeStart);
+sliderOne.addEventListener("touchend", sliderOneSwipeEnd);
+sliderOne.addEventListener("touchmove", sliderSwipeMove);
+sliderOne.addEventListener("touchcancel", sliderSwipeCancel);
+
+
 
 
 /* slider-2 interaction */
@@ -54,6 +142,9 @@ for (let marker of sliderOneMarkersArray) {
 var sliderTwo = document.querySelector(".slider-2");
 var sliderTwoMarkersArray = Array.from(sliderTwo.querySelectorAll(".slider__marker"));
 var sliderTwoItemsArray = Array.from(sliderTwo.querySelectorAll(".slider__item"));
+var sliderTwoBackArrow = sliderTwo.querySelector(".slider__back");
+var sliderTwoWorwardArrow = sliderTwo.querySelector(".slider__forward");
+
 
 //slider-2 marker click handler
 function toggleSliderTwoByMarker() {
@@ -100,3 +191,112 @@ function toggleSliderTwoByMarker() {
 for (let marker of sliderTwoMarkersArray) {
   marker.addEventListener("click", toggleSliderTwoByMarker);
 }
+
+// slider-2 touchscreen interaction
+
+function sliderTwoSwipeEnd() {
+  if (!sliderTouching) {
+    return;
+  }
+
+
+  sliderSwipeLength  = sliderTouchEndX - sliderTouchStartX;
+
+  if (Math.abs(sliderSwipeLength) > (0.33 * document.documentElement.clientWidth)) {
+    if (sliderSwipeLength > 0) {
+      sliderTwoNext();
+    } else {
+      sliderTwoPrevious();
+    }
+  }
+
+  sliderTouching = false;
+}
+
+function sliderTwoNext() {
+  // Make the current active-marker and its corresponding item inactive
+  let activeMarker = sliderTwo.querySelector(".slider__marker.active");
+  let activeItem = sliderTwo.querySelector(".slider__item.active");
+  activeMarker.classList.toggle("active");
+  activeItem.classList.toggle("active");
+
+  // Get index of next item
+  let activeIndex = sliderTwoMarkersArray.indexOf(activeMarker);
+  let targetIndex = (activeIndex + 1) % sliderTwoMarkersArray.length;
+
+  // Make the target marker and its corresponding item active
+  let targetMarker = sliderTwoMarkersArray[targetIndex];
+  let targetItem = sliderTwoItemsArray[targetIndex];
+  targetMarker.classList.toggle("active");
+  targetItem.classList.toggle("active");
+
+  // Clear all marker position classes
+  for (let marker of sliderTwoMarkersArray) {
+    if (marker.classList.contains("first")) {
+      marker.classList.remove("first");
+    } else if (marker.classList.contains("second")) {
+      marker.classList.remove("second");
+    } else if (marker.classList.contains("third")) {
+      marker.classList.remove("third");
+    } else if (marker.classList.contains("fourth")) {
+      marker.classList.remove("fourth");
+    }
+  }
+
+  // Set new marker position classes
+  let markersArrayCopy = sliderTwoMarkersArray.slice();
+  markersArrayCopy.splice(targetIndex, 1);
+  markersArrayCopy[0].classList.add("first");
+  markersArrayCopy[1].classList.add("second");
+  markersArrayCopy[2].classList.add("third");
+  markersArrayCopy[3].classList.add("fourth");
+}
+
+function sliderTwoPrevious() {
+  // Make the current active-marker and its corresponding item inactive
+  let activeMarker = sliderTwo.querySelector(".slider__marker.active");
+  let activeItem = sliderTwo.querySelector(".slider__item.active");
+  activeMarker.classList.toggle("active");
+  activeItem.classList.toggle("active");
+
+  // Get index of next item
+  let activeIndex = sliderTwoMarkersArray.indexOf(activeMarker);
+  let targetIndex = (((activeIndex - 1) % sliderOneMarkersArray.length)
+    + sliderOneMarkersArray.length) % sliderOneMarkersArray.length;
+
+  // Make the target marker and its corresponding item active
+  let targetMarker = sliderTwoMarkersArray[targetIndex];
+  let targetItem = sliderTwoItemsArray[targetIndex];
+  targetMarker.classList.toggle("active");
+  targetItem.classList.toggle("active");
+
+  // Clear all marker position classes
+  for (let marker of sliderTwoMarkersArray) {
+    if (marker.classList.contains("first")) {
+      marker.classList.remove("first");
+    } else if (marker.classList.contains("second")) {
+      marker.classList.remove("second");
+    } else if (marker.classList.contains("third")) {
+      marker.classList.remove("third");
+    } else if (marker.classList.contains("fourth")) {
+      marker.classList.remove("fourth");
+    }
+  }
+
+  // Set new marker position classes
+  let markersArrayCopy = sliderTwoMarkersArray.slice();
+  markersArrayCopy.splice(targetIndex, 1);
+  markersArrayCopy[0].classList.add("first");
+  markersArrayCopy[1].classList.add("second");
+  markersArrayCopy[2].classList.add("third");
+  markersArrayCopy[3].classList.add("fourth");
+}
+
+
+sliderTwo.addEventListener("touchstart", sliderSwipeStart);
+sliderTwo.addEventListener("touchend", sliderTwoSwipeEnd);
+sliderTwo.addEventListener("touchmove", sliderSwipeMove);
+sliderTwo.addEventListener("touchcancel", sliderSwipeCancel);
+
+sliderTwoWorwardArrow.addEventListener("click", sliderTwoNext);
+sliderTwoBackArrow.addEventListener("click", sliderTwoPrevious);
