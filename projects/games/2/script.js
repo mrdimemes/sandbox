1964a3acd6ade9bb0811d9c5fecdcecb145795ea
Dmitry Shagurov <shagurovda@gmail.com>
2021-10-07 20:22:29 +0300

    2048 - basic swipe functions added

--- projects/games/2/script.js
+++ projects/games/2/script.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
             tile = document.createElement("div");
             tile.className = "game-field-tile"
             tile.innerHTML = "0";
-            console.log(tile);
             gameField.appendChild(tile);
             tilesArray.push(tile);
         }
@@ -25,7 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             randomValue = 4;
         }
-
         while (true) {
             randomIndex = Math.floor(Math.random() * 15);
             if (tilesArray[randomIndex].innerHTML == 0) {
@@ -34,6 +32,89 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     }
-
     spawnNewNumber();
+
+    function swipeRight() {
+        let row;
+        let nonZeroRow;
+        let zeroRow;
+        for (let i = 0; i < 4; i++) {
+            row = [
+                parseInt(tilesArray[4 * i].innerHTML),
+                parseInt(tilesArray[4 * i + 1].innerHTML),
+                parseInt(tilesArray[4 * i + 2].innerHTML),
+                parseInt(tilesArray[4 * i + 3].innerHTML)
+            ];
+            nonZeroRow = row.filter(num => num);
+            zeroRow = Array(4 - nonZeroRow.length).fill(0);
+            row = zeroRow.concat(nonZeroRow);
+            tilesArray[4 * i].innerHTML = row[0];
+            tilesArray[4 * i + 1].innerHTML = row[1];
+            tilesArray[4 * i + 2].innerHTML = row[2];
+            tilesArray[4 * i + 3].innerHTML = row[3];
+        }
+    }
+
+    function swipeLeft() {
+        let row;
+        let nonZeroRow;
+        let zeroRow;
+        for (let i = 0; i < 4; i++) {
+            row = [
+                parseInt(tilesArray[4 * i].innerHTML),
+                parseInt(tilesArray[4 * i + 1].innerHTML),
+                parseInt(tilesArray[4 * i + 2].innerHTML),
+                parseInt(tilesArray[4 * i + 3].innerHTML)
+            ];
+            nonZeroRow = row.filter(num => num);
+            zeroRow = Array(4 - nonZeroRow.length).fill(0);
+            row = nonZeroRow.concat(zeroRow);
+            tilesArray[4 * i].innerHTML = row[0];
+            tilesArray[4 * i + 1].innerHTML = row[1];
+            tilesArray[4 * i + 2].innerHTML = row[2];
+            tilesArray[4 * i + 3].innerHTML = row[3];
+        }
+    }
+
+    function swipeUp() {
+        let row;
+        let nonZeroRow;
+        let zeroRow;
+        for (let i = 0; i < 4; i++) {
+            row = [
+                parseInt(tilesArray[i].innerHTML),
+                parseInt(tilesArray[i + 4].innerHTML),
+                parseInt(tilesArray[i + 8].innerHTML),
+                parseInt(tilesArray[i + 12].innerHTML)
+            ];
+            nonZeroRow = row.filter(num => num);
+            zeroRow = Array(4 - nonZeroRow.length).fill(0);
+            row = nonZeroRow.concat(zeroRow);
+            tilesArray[i].innerHTML = row[0];
+            tilesArray[i + 4].innerHTML = row[1];
+            tilesArray[i + 8].innerHTML = row[2];
+            tilesArray[i + 12].innerHTML = row[3];
+        }
+    }
+
+    function swipeDown() {
+        let row;
+        let nonZeroRow;
+        let zeroRow;
+        for (let i = 0; i < 4; i++) {
+            row = [
+                parseInt(tilesArray[i].innerHTML),
+                parseInt(tilesArray[i + 4].innerHTML),
+                parseInt(tilesArray[i + 8].innerHTML),
+                parseInt(tilesArray[i + 12].innerHTML)
+            ];
+            nonZeroRow = row.filter(num => num);
+            zeroRow = Array(4 - nonZeroRow.length).fill(0);
+            row = zeroRow.concat(nonZeroRow);
+            tilesArray[i].innerHTML = row[0];
+            tilesArray[i + 4].innerHTML = row[1];
+            tilesArray[i + 8].innerHTML = row[2];
+            tilesArray[i + 12].innerHTML = row[3];
+        }
+    }
 })
